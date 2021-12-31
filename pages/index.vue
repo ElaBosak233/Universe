c061a62ab59089bd57395539c52cbeec21b9d520
埃拉拉 <ElaBosak233@qq.com>
2021-12-31 23:23:01 +0800

    Loading...

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,11 @@
-<template>
-    <div class="max-w-sm mx-auto mt-4">
-        <div class="flex items-center">
-            <img
-                class="w-40 h-40 rounded-full mb-4"
-                height="160"
-                width="160"
-                src="https://i.loli.net/2021/03/13/RSLyWDlwuci1atf.jpg"
-                alt="ElaBosak233"
-            />
-            <a
-                target="blank"
-                class="ml-4 text-green-900 dark:text-green-400 font-bold"
-                href="https://github.com/ElaBosak233"
-            >Follow Me Github</a>
-        </div>
-        <ul role="list" class="mt-10 space-y-4">
-            <li v-for="item in lists" :key="item.id">
-                <NuxtLink class="py-4 hover:shadow-xl global-text" :to="item.url">
-                    <div
-                        class="flex items-center justify-between space-x-3 bg-gray-300 dark:bg-gray-700 rounded-lg p-4"
-                    >
-                        {{ item.title }}
-                        <ArrowNarrowRightIcon class="h-6 w-6 global-text" aria-hidden="true" />
-                    </div>
-                </NuxtLink>
-            </li>
-        </ul>
-    </div>
-</template>
-
 <script lang="ts">
-import { ArrowNarrowRightIcon } from "@heroicons/vue/outline/index.js";
-
-interface links {
-    id: number
-    title: string
-    url: any
-}
-
 export default {
-    components: {
-        ArrowNarrowRightIcon
-    },
-    setup() {
-        useMeta({
-            title: "首页 - 埃拉の柚子"
-        });
-        const router = useRouter()
-        const lists = ref<links[]>([
-            { id: 1, title: 'Pinia Store', url: 'pinia' },
-            { id: 1, title: 'Modal', url: 'modal' },
-            { id: 1, title: 'Menu', url: 'menu' },
-            { id: 1, title: 'New soon...', url: '' },
-        ])
-        return {
-            lists
-        }
-    }
+    layout: "index"
 }
 </script>
+
+<style lang="scss">
+body {
+    @apply bg-orange-400
+}
+</style>>
